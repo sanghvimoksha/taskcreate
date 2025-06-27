34837d1070e6ca88d89a52f82e6024ed25774b49
Moksha Sanghvi <111114487+sanghvimoksha@users.noreply.github.com>
2025-06-27 15:49:35 +0530

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+interface Task {
+  id: number;
+  task: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'task-create';
+    task="";
+    taskList:{id: number, task: string}[]=[]
+
+    addTask(){
+     this.taskList.push({id:this.taskList.length+1, task:this.task})
+     this.task=""
+
+     console.log(this.taskList);
+    }
+
+    deleteTask(taskId: number){
+    this.taskList= this.taskList.filter((item)=>item.id!=taskId);
+    }
 }
